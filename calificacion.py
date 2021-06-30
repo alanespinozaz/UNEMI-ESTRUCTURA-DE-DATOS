57de2891848a9912353fb866cdc59a1934770bd6
Alan Espinoza <85721445+alanespinozaz@users.noreply.github.com>
2021-06-29 22:02:19 -0500

    Add files via upload

---
+++ calificacion.py
@@ -0,0 +1,15 @@
+"" "Dado como dato la calificación de un alumno en un examen escriba “aprobado” si su calificación es mayor o igual que 7 y “Reprobado” en caso contrario. "" "
+
+clase  Ejemplo4 :
+    def  __init__ ( yo ):
+        aprobar
+    
+    def  aporepro ( yo ):
+        cal  =  float ( input ( "Ingrese su Nota final:" ))
+        si  cal  > =  7 :
+            print ( "Felicitaciones usted ha aprobado el curso {}" . formato ( cal ))   
+        otra cosa :
+            print ( "Usted ha Reprobado el curso {}" . formato ( cal ))
+        
+eje1=Ejemplo4 ()
+eje1.aporepro ()
